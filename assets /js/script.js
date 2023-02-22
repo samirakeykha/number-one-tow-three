2d9c407d52face2cab27f1353d5759585091a5f2
samira keykha <sami.key35@gmail.com>
2023-02-22 19:12:27 +0000

    Set up structure of game functions

--- assets /js/script.js
+++ assets /js/script.js
@@ -1,1 +1,31 @@
-console.log("connected!")
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
